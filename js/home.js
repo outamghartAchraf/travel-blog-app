c7258c992ef2cb3f4e421485584caf7410cf6d68
outamghartAchraf <outamghart2000@gmail.com>
2026-02-03 10:22:11 +0100

    add code of dom elements

---
+++ js/home.js
@@ -0,0 +1,15 @@
+
+// =====================
+// DOM Elements
+// =====================
+const tripsContainer = document.getElementById("trips-container");
+const filterContainer = document.getElementById("category-filters");
+const modal = document.getElementById("trip-modal");
+const form = document.getElementById("trip-form");
+const closeModalBtn = document.getElementById("close-modal");
+
+const titleInput = document.getElementById("title");
+const imageInput = document.getElementById("image");
+const countryInput = document.getElementById("country");
+const categoryInput = document.getElementById("category");
+
