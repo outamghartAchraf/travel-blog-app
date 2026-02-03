abf457da7221bc817e9f960051650c826f743a87
outamghartAchraf <outamghart2000@gmail.com>
2026-02-03 10:23:22 +0100

    add data inisial array

--- js/home.js
+++ js/home.js
@@ -13,3 +13,38 @@ const imageInput = document.getElementById("image");
 const countryInput = document.getElementById("country");
 const categoryInput = document.getElementById("category");
 
+// =====================
+// Data 
+// =====================
+let trips = [
+  {
+    id: 1,
+    title: "Yellow River",
+    country: "China",
+    image: "../assets/images/acticle1.jpg",
+    category: "River",
+    time: "15 min",
+    likes: "12k",
+    author: "Ian Jachson"
+  },
+  {
+    id: 2,
+    title: "Summer England",
+    country: "England",
+    image: "../assets/images/acticle2.jpg",
+    category: "Forest",
+    time: "15 min",
+    likes: "12k",
+    author: "Ian Jachson"
+  },
+  {
+    id: 3,
+    title: "Sunset Beach",
+    country: "Maldives",
+    image: "../assets/images/acticle3.jpg",
+    category: "Beach",
+    time: "15 min",
+    likes: "12k",
+    author: "Ian Jachson"
+  }
+];
